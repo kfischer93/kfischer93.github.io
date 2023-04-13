4ebdda2e464aed6cadcae6479abf9ae04dde3cf5
Katie Fischer <kafi7166@colorado.edu>
2023-04-12 23:37:35 -0600

    Update balls.js

--- js/balls.js
+++ js/balls.js
@@ -4,72 +4,82 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-// function to generate random number
+// Array of available colors
+const colors = [
+  'rgb(192, 238, 228)',
+  'rgb(248, 249, 136)',
+  'rgb(255, 202, 200)',
+  'rgb(255, 158, 158)',
+  'rgb(16, 161, 157)',
+  'rgb(179, 201, 156)',
+  'rgb(213, 180, 180)'
+];
 
+// function to generate random number
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
 
-// function to generate random color
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+// function to randomly select one of the available colors
+function randomColor() {
+  return colors[random(0, colors.length - 1)];
 }
 
 class Ball {
-    constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
-      this.color = color;
-      this.size = size;
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
     }
-    draw() {
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-        ctx.fill();
-      }
-      
-      update() {
-        if ((this.x + this.size) >= width) {
-          this.velX = -(this.velX);
-        }
-      
-        if ((this.x - this.size) <= 0) {
-          this.velX = -(this.velX);
-        }
-      
-        if ((this.y + this.size) >= height) {
-          this.velY = -(this.velY);
-        }
-      
-        if ((this.y - this.size) <= 0) {
-          this.velY = -(this.velY);
-        }
-      
-        this.x += this.velX;
-        this.y += this.velY;
-      }
 
-      collisionDetect() {
-        for (const ball of balls) {
-          if (this !== ball) {
-            const dx = this.x - ball.x;
-            const dy = this.y - ball.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-      
-            if (distance < this.size + ball.size) {
-              ball.color = this.color = randomRGB();
-            }
-          }
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomColor();
         }
       }
+    }
+  }
 }
-const testBall = new Ball(100, 300, 4, 4, "pink", 10);
 
+const testBall = new Ball(100, 300, 4, 4, "pink", 10);
 
 testBall.x;
 testBall.size;
@@ -77,7 +87,6 @@ testBall.color;
 testBall.draw();
 
 const balls = [];
-const colors = ["#e2a47a", "#9aae8f", "#ffbeb3", "#b36880", "#f2bc83"];
 
 while (balls.length < 25) {
   const size = random(5, 30);
@@ -88,7 +97,7 @@ while (balls.length < 25) {
     random(0 + size, height - size),
     random(-5, 5),
     random(-5, 5),
-    colors.random(),
+    randomColor(),
     size
   );
 
@@ -96,16 +105,16 @@ while (balls.length < 25) {
 }
 
 function loop() {
-    ctx.fillStyle = "rgba(255, 255, 255)";
-    ctx.fillRect(0, 0, width, height);
-  
-    for (const ball of balls) {
-      ball.draw();
-      ball.update();
-      ball.collisionDetect();
-    }
-  
-    requestAnimationFrame(loop);
+  ctx.fillStyle = "rgba(255, 255, 255)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
   }
 
-  loop();
+  requestAnimationFrame(loop);
+}
+
+loop();
